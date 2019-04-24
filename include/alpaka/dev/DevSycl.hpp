cb71fb40af644d2eaef8d6026a2ba0487400d486
Jan Stephan <jan.stephan.dd@gmail.com>
2019-04-24 20:34:25 +0200

    Added SYCL to dev

---
+++ include/alpaka/dev/DevSycl.hpp
@@ -0,0 +1,227 @@
+/* Copyright 2019 Jan Stephan
+ *
+ * This file is part of Alpaka.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+ #pragma once
+
+#ifdef ALPAKA_ACC_SYCL_ENABLED
+
+#include <alpaka/core/Common.hpp>
+
+#if !BOOST_LANG_SYCL
+    #error If ALPAKA_ACC_SYCL_ENABLED is set, the compiler has to support SYCL!
+#endif
+
+#include <alpaka/dev/Traits.hpp>
+#include <alpaka/mem/buf/Traits.hpp>
+#include <alpaka/pltf/Traits.hpp>
+#include <alpaka/wait/Traits.hpp>
+
+#include <alpaka/core/Sycl.hpp>
+
+namespace alpaka
+{
+    namespace pltf
+    {
+        namespace traits
+        {
+            template<
+                typename TPltf,
+                typename TSfinae>
+            struct GetDevByIdx;
+        }
+        class PltfSycl;
+    }
+
+    namespace dev
+    {
+        //#############################################################################
+        //! The SYCL device handle.
+        class DevSycl
+        {
+            friend struct pltf::traits::GetDevByIdx<pltf::PltfSycl>;
+
+        protected:
+            //-----------------------------------------------------------------------------
+            DevSycl() = default;
+        public:
+            //-----------------------------------------------------------------------------
+            DevSycl(DevSycl const &) = default;
+            //-----------------------------------------------------------------------------
+            DevSycl(DevSycl &&) = default;
+            //-----------------------------------------------------------------------------
+            auto operator=(DevSycl const &) -> DevSycl & = default;
+            //-----------------------------------------------------------------------------
+            auto operator=(DevSycl &&) -> DevSycl & = default;
+            //-----------------------------------------------------------------------------
+            ALPAKA_FN_HOST auto operator==(DevSycl const & rhs) const
+            -> bool
+            {
+                return m_Device.get() == rhs.m_Device.get();
+            }
+            //-----------------------------------------------------------------------------
+            ALPAKA_FN_HOST auto operator!=(DevSycl const & rhs) const
+            -> bool
+            {
+                return !((*this) == rhs);
+            }
+            //-----------------------------------------------------------------------------
+            ~DevSycl() = default;
+
+        public:
+            cl::sycl::device m_Device;
+            cl::sycl::queue m_Queue;
+        };
+    }
+
+    namespace dev
+    {
+        namespace traits
+        {
+            //#############################################################################
+            //! The SYCL device name get trait specialization.
+            template<>
+            struct GetName<
+                dev::DevSycl>
+            {
+                //-----------------------------------------------------------------------------
+                ALPAKA_FN_HOST static auto getName(
+                    dev::DevSycl const & dev)
+                -> std::string
+                {
+                    // get_info returns std::string in this case
+                    return dev.m_Device.get_info<
+                        cl::sycl::info::device::name>();
+                }
+            };
+
+            //#############################################################################
+            //! The SYCL device available memory get trait specialization.
+            template<>
+            struct GetMemBytes<
+                dev::DevSycl>
+            {
+                //-----------------------------------------------------------------------------
+                ALPAKA_FN_HOST static auto getMemBytes(
+                    dev::DevSycl const & dev)
+                -> std::size_t
+                {
+                    // get_info returns cl_ulong in this case
+                    return dev.m_Device.get_info<
+                        cl::sycl::info::device::global_mem_size>();
+                }
+            };
+
+            //#############################################################################
+            //! The SYCL device free memory get trait specialization. Note that
+            //! this function will always return 0 as there is no way in SYCL
+            //! or OpenCL to query free memory.
+            template<>
+            struct GetFreeMemBytes<
+                dev::DevSycl>
+            {
+                //-----------------------------------------------------------------------------
+                ALPAKA_FN_HOST static auto getFreeMemBytes(
+                    dev::DevSycl const & dev)
+                -> std::size_t
+                {
+                    // FIXME: There is no way in either SYCL or OpenCL to
+                    // query free memory.
+                    return 0;
+                }
+            };
+
+            //#############################################################################
+            //! The SYCL device reset trait specialization. Note that this
+            //! function won't actually do anything. If you need to reset your
+            //! SYCL device its destructor must be called.
+            template<>
+            struct Reset<
+                dev::DevSycl>
+            {
+                //-----------------------------------------------------------------------------
+                ALPAKA_FN_HOST static auto reset(
+                    dev::DevSycl const & dev)
+                -> void
+                {
+                    ALPAKA_DEBUG_FULL_LOG_SCOPE;
+                }
+            };
+        }
+    }
+    namespace mem
+    {
+        namespace buf
+        {
+            template<
+                typename TElem,
+                typename TDim,
+                typename TIdx>
+            class BufSycl;
+
+            namespace traits
+            {
+                //#############################################################################
+                //! The SYCL device memory buffer type trait specialization.
+                template<
+                    typename TElem,
+                    typename TDim,
+                    typename TIdx>
+                struct BufType<
+                    dev::DevSycl,
+                    TElem,
+                    TDim,
+                    TIdx>
+                {
+                    using type = mem::buf::BufSycl<TElem, TDim, TIdx>;
+                };
+            }
+        }
+    }
+    namespace pltf
+    {
+        namespace traits
+        {
+            //#############################################################################
+            //! The SYCL device platform type trait specialization.
+            template<>
+            struct PltfType<
+                dev::DevSycl>
+            {
+                using type = pltf::PltfSycl;
+            };
+        }
+    }
+    namespace wait
+    {
+        namespace traits
+        {
+            //#############################################################################
+            //! The thread SYCL device wait specialization.
+            //!
+            //! Blocks until the device has completed all preceding requested tasks.
+            //! Tasks that are enqueued or queues that are created after this call is made are not waited for.
+            template<>
+            struct CurrentThreadWaitFor<
+                dev::DevSycl>
+            {
+                //-----------------------------------------------------------------------------
+                ALPAKA_FN_HOST static auto currentThreadWaitFor(
+                    dev::DevSycl const & dev)
+                -> void
+                {
+                    ALPAKA_DEBUG_FULL_LOG_SCOPE;
+
+                    dev.m_Queue.wait_and_throw();
+                }
+            };
+        }
+    }
+}
+
+#endif
