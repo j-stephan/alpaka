23aa37366841745cb5fef6751b2643657b99d4ab
Jan Stephan <j.stephan@hzdr.de>
2019-09-12 17:57:15 +0200

    Fix undefined behaviour
    Replaced a few const_cast calls which resulted in undefined behaviour.
    SYCL objects are reference-counted, so we can just copy from const
    references.

--- include/alpaka/mem/buf/sycl/Copy.hpp
+++ include/alpaka/mem/buf/sycl/Copy.hpp
@@ -66,14 +66,14 @@ namespace alpaka
                     {
                         auto operator()(cl::sycl::handler& cgh) -> void
                         {
-                            auto dst_acc = dst_buf->template get_access<
+                            auto dst_acc = dst_buf.template get_access<
                                 cl::sycl::access::mode::write,
                                 cl::sycl::access::target::global_buffer>(cgh, range);
                             cgh.copy(src_ptr, dst_acc);
                         }
 
                         const TElem * const src_ptr;
-                        cl::sycl::buffer<TElem, TDim::value>* dst_buf;
+                        cl::sycl::buffer<TElem, TDim::value> dst_buf;
                         cl::sycl::range<TDim::value> range;
                     };
 
@@ -84,12 +84,12 @@ namespace alpaka
                     {
                         auto operator()(cl::sycl::handler& cgh) -> void
                         {
-                            auto src_acc = src_buf->template get_access<
+                            auto src_acc = src_buf.template get_access<
                                             cl::sycl::access::mode::read>(cgh, range);
                             cgh.copy(src_acc, dst_ptr);
                         }
 
-                        cl::sycl::buffer<TElem, TDim::value>* src_buf;
+                        cl::sycl::buffer<TElem, TDim::value> src_buf;
                         TElem * const dst_ptr;
                         cl::sycl::range<TDim::value> range;
                     };
@@ -101,16 +101,16 @@ namespace alpaka
                     {
                         auto operator()(cl::sycl::handler& cgh) -> void
                         {
-                            auto src_acc = src_buf->template get_access<
+                            auto src_acc = src_buf.template get_access<
                                             cl::sycl::access::mode::read>(cgh, range);
-                            auto dst_acc = dst_buf->template get_access<
+                            auto dst_acc = dst_buf.template get_access<
                                             cl::sycl::access::mode::write>(cgh, range);
 
                             cgh.copy(src_acc, dst_acc);
                         }
 
-                        cl::sycl::buffer<TElem, TDim::value>* src_buf;
-                        cl::sycl::buffer<TElem, TDim::value>* dst_buf;
+                        cl::sycl::buffer<TElem, TDim::value> src_buf;
+                        cl::sycl::buffer<TElem, TDim::value> dst_buf;
                         cl::sycl::range<TDim::value> range;
                     };
                 }
@@ -160,9 +160,7 @@ namespace alpaka
                             elem::Elem<std::remove_const_t<TViewSrc>>,
                             TDim,
                             mem::view::sycl::detail::copy_type::device_to_host>{
-                                // This is really dumb - buffers can never be const in SYCL, even when
-                                // in read-only mode
-                                const_cast<std::remove_const_t<decltype(viewSrc.m_buf)>*>(&viewSrc.m_buf),
+                                viewSrc.m_buf,
                                 mem::view::getPtrNative(viewDst),
                                 mem::view::sycl::detail::get_sycl_range<dim::Dim<TExtent>::value>(extent)
                             };
@@ -211,7 +209,7 @@ namespace alpaka
                             TDim,
                             mem::view::sycl::detail::copy_type::host_to_device>{
                                 mem::view::getPtrNative(viewSrc),
-                                &viewDst.m_buf,
+                                viewDst.m_buf,
                                 mem::view::sycl::detail::get_sycl_range<dim::Dim<TExtent>::value>(extent)
                             };
                     }
@@ -257,10 +255,8 @@ namespace alpaka
                             elem::Elem<std::remove_const_t<TViewSrc>>,
                             TDim,
                             mem::view::sycl::detail::copy_type::device_to_device>{
-                                // This is really dumb - buffers can never be const in SYCL, even when
-                                // in read-only mode
-                                const_cast<std::remove_const_t<decltype(viewSrc.m_buf)>*>(&viewSrc.m_buf),
-                                &viewDst.m_buf,
+                                viewSrc.m_buf,
+                                viewDst.m_buf,
                                 mem::view::sycl::detail::get_sycl_range<dim::Dim<TExtent>::value>(extent)
                             };
                     }
