421fe615faecac2200f8c08cf9ea70552be1f9d2
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2021-02-23 21:35:25 +0100

    add example for an accessor with a projection

--- test/unit/mem/view/src/Accessor.cpp
+++ test/unit/mem/view/src/Accessor.cpp
@@ -162,3 +162,126 @@ TEST_CASE("customPointer", "[accessor]")
     alpaka::exec<Acc>(queue, workdiv, ReadKernelExplicit{}, readAccessor);
     alpaka::exec<Acc>(queue, workdiv, ReadWriteKernelExplicit{}, readWriteAccessor);
 }
+
+namespace
+{
+    template<typename Projection, typename MemoryHandle, typename Elem, typename BufferIdx, std::size_t Dim>
+    struct AccessorWithProjection;
+
+    template<typename Projection, typename Pointer, typename Elem, typename BufferIdx>
+    struct AccessorWithProjection<Projection, Pointer, Elem, BufferIdx, 1>
+    {
+        AccessorWithProjection(alpaka::Accessor<MemoryHandle, Elem, BufferIdx, 1, alpaka::ReadAccess> accessor)
+            : accessor(accessor)
+        {
+        }
+
+        ALPAKA_FN_ACC auto operator[](alpaka::Vec<alpaka::DimInt<1>, BufferIdx> i) const -> Elem
+        {
+            return Projection{}(accessor[i]);
+        }
+
+        ALPAKA_FN_ACC auto operator[](BufferIdx i) const -> Elem
+        {
+            return Projection{}(accessor[i]);
+        }
+
+        ALPAKA_FN_ACC auto operator()(BufferIdx i) const -> Elem
+        {
+            return Projection{}(accessor(i));
+        }
+
+        alpaka::Accessor<MemoryHandle, Elem, BufferIdx, 1, alpaka::ReadAccess> accessor;
+    };
+
+    template<typename Projection, typename Pointer, typename Elem, typename BufferIdx>
+    struct AccessorWithProjection<Projection, Pointer, Elem, BufferIdx, 2>
+    {
+        AccessorWithProjection(alpaka::Accessor<MemoryHandle, Elem, BufferIdx, 2, alpaka::ReadAccess> accessor)
+            : accessor(accessor)
+        {
+        }
+
+        ALPAKA_FN_ACC auto operator[](alpaka::Vec<alpaka::DimInt<2>, BufferIdx> i) const -> Elem
+        {
+            return Projection{}(accessor[i]);
+        }
+
+        ALPAKA_FN_ACC auto operator()(BufferIdx y, BufferIdx x) const -> Elem
+        {
+            return Projection{}(accessor(y, x));
+        }
+
+        alpaka::Accessor<MemoryHandle, Elem, BufferIdx, 2, alpaka::ReadAccess> accessor;
+    };
+
+    template<typename Projection, typename Pointer, typename Elem, typename BufferIdx>
+    struct AccessorWithProjection<Projection, Pointer, Elem, BufferIdx, 3>
+    {
+        AccessorWithProjection(alpaka::Accessor<MemoryHandle, Elem, BufferIdx, 3, alpaka::ReadAccess> accessor)
+            : accessor(accessor)
+        {
+        }
+
+        ALPAKA_FN_ACC auto operator[](alpaka::Vec<alpaka::DimInt<3>, BufferIdx> i) const -> Elem
+        {
+            return Projection{}(accessor[i]);
+        }
+
+        ALPAKA_FN_ACC auto operator()(BufferIdx z, BufferIdx y, BufferIdx x) const -> Elem
+        {
+            return Projection{}(accessor(z, y, x));
+        }
+
+        alpaka::Accessor<MemoryHandle, Elem, BufferIdx, 3, alpaka::ReadAccess> accessor;
+    };
+
+    struct DoubleValue
+    {
+        auto operator()(int i) const
+        {
+            return i * 2;
+        }
+    };
+
+    struct CopyKernel
+    {
+        template<typename Acc, typename Pointer, typename Idx>
+        ALPAKA_FN_ACC void operator()(
+            Acc const&,
+            alpaka::Accessor<Pointer, int, Idx, 1, alpaka::ReadAccess> const src,
+            alpaka::Accessor<Pointer, int, Idx, 1, alpaka::WriteAccess> const dst) const
+        {
+            auto const projSrc = AccessorWithProjection<DoubleValue>{src};
+            dst[0] = projSrc[0];
+        }
+    };
+} // namespace
+
+TEST_CASE("projection", "[accessor]")
+{
+    using Dim = alpaka::DimInt<1>;
+    using Size = std::size_t;
+    using Acc = alpaka::ExampleDefaultAcc<Dim, Size>;
+    using DevAcc = alpaka::Dev<Acc>;
+    using Queue = alpaka::Queue<DevAcc, alpaka::Blocking>;
+
+    auto const devAcc = alpaka::getDevByIdx<Acc>(0u);
+    auto queue = Queue{devAcc};
+
+    auto srcBuffer = alpaka::allocBuf<int, Size>(devAcc, Size{1});
+    auto dstBuffer = alpaka::allocBuf<int, Size>(devAcc, Size{1});
+
+    std::array<int, 1> host{{42}};
+    alpaka::memcpy(queue, srcBuffer, host, 1);
+
+    const auto workdiv = alpaka::WorkDivMembers<Dim, Size>{
+        alpaka::Vec<Dim, Size>{Size{1}},
+        alpaka::Vec<Dim, Size>{Size{1}},
+        alpaka::Vec<Dim, Size>{Size{1}}};
+    alpaka::exec<Acc>(queue, workdiv, CopyKernel{}, alpaka::readAccess(srcBuffer), alpaka::writeAccess(dstBuffer));
+
+    alpaka::memcpy(queue, host, dstBuffer, 1);
+
+    REQUIRE(host[0] == 84);
+}
